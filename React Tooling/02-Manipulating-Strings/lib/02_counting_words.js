f862d2be93b301fd1ca35b2fbd4b0974262a36dd
luisaugusto <luis.augusto.u@gmail.com>
2022-02-14 17:50:09 -0500

    se termino el ejercicio

--- React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,12 @@
 function occurrences(text, word) {
-  return null;
+  const result = text.replace(/[^a-zA-Z ]/g, "").split(" ");
+  let sum = 0;
+  result.forEach((palabra) => {
+    if (palabra.toLowerCase() === word.toLowerCase()) {
+      sum += 1;
+    }
+  });
+  return sum;
 }
-
+console.log(occurrences("a fat, cat sat on a mat and ate a fat, rat", "fat"));
 module.exports = occurrences;
