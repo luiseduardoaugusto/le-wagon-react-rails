d3e3af0dfddeccf0637f86d14d963dea2744972c
luisaugusto <luis.augusto.u@gmail.com>
2022-02-14 16:04:48 -0500

    ejercicio

--- React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,22 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  const palabras = word.split("");
+  const vocals = ["a", "e", "i", "o", "u", " "];
+  let resultado = '';
+  let anterior;
+  palabras.forEach((letra) => {
+    if (!vocals.includes(letra.toLowerCase())) {
+      if (anterior === false) {
+        resultado += `-${letra}`;
+        return;
+      }
+      anterior = false;
+    } else {
+      anterior = true;
+    }
+    resultado += letra;
+  });
+  return resultado;
 }
 
 module.exports = insertDash;
